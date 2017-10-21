0a9124d6ef1587fa5a871cc83595f4d55478952e
florian.bramer <florian.bramer@esrlabs.com>
2017-10-21 19:58:58 +0200

    moved "untagged" to the bginning of the filter dialog

--- src/view/TracksView.js
+++ src/view/TracksView.js
@@ -10,9 +10,9 @@ const Filters = require('./Filters.js');
 const util = require('util');
 const TagDialog = require('./TagDialog.js');
 
-class TracksView{
+class TracksView {
 
-  constructor(screen, style, player, playlistManager){
+  constructor(screen, style, player, playlistManager) {
     this._screen = screen;
     this._style = style;
     this._player = player;
@@ -44,8 +44,8 @@ class TracksView{
         }
       },
       style: JSON.parse(JSON.stringify(style.box)), //deep copy style
-      search: function(callback) {
-        self._prompt.input('{white-fg}Search track:{/white-fg}', '', function(err, value) {
+      search: function (callback) {
+        self._prompt.input('{white-fg}Search track:{/white-fg}', '', function (err, value) {
           if (err) return;
           return callback(null, value);
         });
@@ -71,35 +71,35 @@ class TracksView{
     this._updateTitle();
   }
 
-  showList(list){
+  showList(list) {
     this._trackList = list;
     this._update();
   }
 
-  show(){
+  show() {
     this._list.focus();
     this._list.enterSelected(0);
     this._list.show();
     this._screen.render();
   }
 
-  hide(){
+  hide() {
     this._list.hide();
     this._screen.render();
   }
 
-  focus(){
+  focus() {
     this._list.focus();
   }
 
-  onPlay(){
+  onPlay() {
     let track = this._selectedTrack();
     logger.debug('play ' + track.getName());
 
-    this._player.play(this._filteredTrackList, this._list.selected); 
+    this._player.play(this._filteredTrackList, this._list.selected);
   }
 
-  onFocused(){
+  onFocused() {
     this._updateTitle();
     this._screen.key('p', this.onPlay.bind(this));
     this._screen.key('C-c', this.copyToClipboard.bind(this));
@@ -108,7 +108,7 @@ class TracksView{
     this._screen.key('i', this.showInfo.bind(this));
   }
 
-  onUnFocused(){
+  onUnFocused() {
     this._updateTitle();
     this._screen.removeKeyAll('p');
     this._screen.removeKeyAll('C-c');
@@ -117,38 +117,38 @@ class TracksView{
     this._screen.removeKeyAll('i');
   }
 
-  copyToClipboard(){
+  copyToClipboard() {
     let track = this._selectedTrack();
-    if (!track){
+    if (!track) {
       return; //nothing selected
     }
 
     logger.verbose('copy ' + track.getName());
 
-    clipboard.set(track, (clipboard, playlist, selectedPlaylistEntry)=>{
+    clipboard.set(track, (clipboard, playlist, selectedPlaylistEntry) => {
       playlist.addTrack(clipboard.content(), selectedPlaylistEntry);
     });
   }
 
-  filter(){
+  filter() {
     let dialog = new CheckboxListDialog(this._screen, this._style, 'f');
     let availableTags = tagConfig.getQualifiedTags();
     const UNTAGGED_OPTION = '[untagged]';
-    availableTags.push(UNTAGGED_OPTION);
+    availableTags.unshift(UNTAGGED_OPTION);
 
-    let usedTags = [...this._activeFilters].map((filter)=>filter.getName());
+    let usedTags = [...this._activeFilters].map((filter) => filter.getName());
 
-    dialog.ask('Filter by tags', availableTags, usedTags, (addedElements, removedElements, selectedTags)=>{
-      if (!selectedTags){
+    dialog.ask('Filter by tags', availableTags, usedTags, (addedElements, removedElements, selectedTags) => {
+      if (!selectedTags) {
         return; //user pressed esc
       }
 
       this._activeFilters.clear();
 
-      selectedTags.forEach((tagName)=>{
+      selectedTags.forEach((tagName) => {
         this._activeFilters.add(
-          tagName === UNTAGGED_OPTION 
-            ? new Filters.FilterUntagged(UNTAGGED_OPTION) 
+          tagName === UNTAGGED_OPTION
+            ? new Filters.FilterUntagged(UNTAGGED_OPTION)
             : new Filters.FilterByTag(tagName)
         );
       });
@@ -157,28 +157,28 @@ class TracksView{
     });
   }
 
-  tagEntry(){
+  tagEntry() {
     let track = this._selectedTrack();
-    if (!track){
+    if (!track) {
       return; //nothing selected
     }
 
     let usedTags = track.tagsAsString();
-    
+
     let dialog = new TagDialog(this._screen, this._style, 't', 'Select tags');
-    dialog.ask(usedTags, (addedElements, removedElements)=>{
-      addedElements.forEach((tagName)=>{
+    dialog.ask(usedTags, (addedElements, removedElements) => {
+      addedElements.forEach((tagName) => {
         this._playlistManager.addTrackToTag(track, tagName);
       });
-      removedElements.forEach((tagName)=>{
+      removedElements.forEach((tagName) => {
         this._playlistManager.removeTrackFromTag(track, tagName);
       });
     });
   }
 
-  showInfo(){
+  showInfo() {
     let track = this._selectedTrack();
-    if (!track){
+    if (!track) {
       return; //nothing selected
     }
 
@@ -186,38 +186,38 @@ class TracksView{
     view.show();
   }
 
-  _selectedTrack(){
+  _selectedTrack() {
     return this._filteredTrackList[this._list.selected];
   }
 
-  _updateTitle(){
+  _updateTitle() {
     let template = this._list.focused ? this._style.title.focused : this._style.title.unfocused;
-    let filters = this._activeFilters.size == 0 
+    let filters = this._activeFilters.size == 0
       ? 'none'
-      : [...this._activeFilters].map((f)=>f.getName()).join(',');
+      : [...this._activeFilters].map((f) => f.getName()).join(',');
     let nrOfEntries = this._filteredTrackList ? this._filteredTrackList.length : 0;
-    this._list.setLabel(template.replace('${title}','All tracks (' +  nrOfEntries + ')[filters=' + filters + ']'));
+    this._list.setLabel(template.replace('${title}', 'All tracks (' + nrOfEntries + ')[filters=' + filters + ']'));
 
     this._screen.render();
   }
 
-  _update(){
+  _update() {
     let trackStyle = this._style.track;
     let allTracks = this._trackList.getTracks();
     this._filteredTrackList = allTracks;
-    
-    if (this._activeFilters.size > 0){
+
+    if (this._activeFilters.size > 0) {
       this._filteredTrackList = allTracks.filter((t) => {
-        return [...this._activeFilters].filter(f=>f.matches(t)).length > 0;
+        return [...this._activeFilters].filter(f => f.matches(t)).length > 0;
       });
     }
 
-    let items = this._filteredTrackList.map( (element) => {
+    let items = this._filteredTrackList.map((element) => {
       let str = trackStyle
-        .replace('${artist}',element.getArtist())
-        .replace('${title}',element.getTitle());
+        .replace('${artist}', element.getArtist())
+        .replace('${title}', element.getTitle());
       return str;
-    } );
+    });
 
     this._list.setItems(items || ['<empty>']);
 
